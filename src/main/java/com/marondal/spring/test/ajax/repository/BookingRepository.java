f59400047f6bd98ccbd16556086f7ff211bc873b
megait <megait@gmail.com>
2024-08-26 20:56:36 +0900

    [김인규]  - 예약하기 조회, 입력 기능 풀이

---
+++ src/main/java/com/marondal/spring/test/ajax/repository/BookingRepository.java
@@ -0,0 +1,25 @@
+package com.marondal.spring.test.ajax.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.marondal.spring.test.ajax.domain.Booking;
+
+@Mapper
+public interface BookingRepository {
+	
+	public List<Booking> selectBookingList();
+	
+	public int insertBooking(
+			@Param("name") String name
+			, @Param("date") LocalDate date
+			, @Param("day") int day
+			, @Param("headcount") int headcount
+			, @Param("phoneNumber") String phoneNumber
+			, @Param("state") String state);
+	
+
+}
